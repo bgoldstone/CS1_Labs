7517f8f61dce3061797d6e9676cca4f167b0f407
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-14 17:14:23 -0400

    Removed Length Variable

--- Labs/3_listTools.py
+++ Labs/3_listTools.py
@@ -4,14 +4,12 @@
 # Date: 9/8/2020
 listOfNumbers = [10, -5, 14, 0, 47, -12, 17, -4]
 total = 0
-length = 0
 for number in listOfNumbers:
     total += number #adds previous number or initial number to total each time
-    length += 1 #adds one each time to count numbers
-average = total / length #divides total by length to get average
+average = total / len(listOfNumbers) #divides total by length to get average
 #prints output
 print(f"The List is: {listOfNumbers}")
 print()
-print(f"Count   = {length}")
+print(f"Count   = {len(listOfNumbers)}")
 print(f"Sum     = {total}")
 print(f"Average = {average}")
